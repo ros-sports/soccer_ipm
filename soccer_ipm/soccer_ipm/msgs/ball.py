79299fb5f31fa07f8341f5681ee04738a16e7a03
Florian Vahl <florian@flova.de>
2022-11-01 13:27:41 +0100

    Update soccer_ipm/soccer_ipm/msgs/ball.py
    Co-authored-by: Jan Gutsche <34797331+jaagut@users.noreply.github.com>
    Signed-off-by: Florian Vahl <florian@flova.de>

--- soccer_ipm/soccer_ipm/msgs/ball.py
+++ soccer_ipm/soccer_ipm/msgs/ball.py
@@ -58,8 +58,6 @@ def map_ball_array(
             balls_relative.balls.append(ball_relative)
         except NoIntersectionError:
             logger.warn(
-                'Got a ball at ({},{}) I could not transform.'.format(
-                    ball.center.x,
-                    ball.center.y),
+                f'Could not transform ball at ({ball.center.x},{ball.center.y}).',
                 throttle_duration_sec=5)
     return balls_relative
